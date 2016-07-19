3e20c594d32a8a99f389105a25ae980e3a118be4
Marcellus Easley <marcellus.easley@gmail.com>
2016-07-19 14:17:20 -0400

    added all possible functions to example script

--- zipfreeinfo/exampleapp.py
+++ zipfreeinfo/exampleapp.py
@@ -1,8 +1,18 @@
 import zipfreeinfo
 
 
-print(zipfreeinfo.get_state())
+zipfreeinfo.set_auth_credentials('your auth id','your auth token')
+
+# Returns a dictionary with nested data structures containing zip code data
+# Also sets internal zipdata structure with zip code data, which can be returned with function calls
 print(zipfreeinfo.get_zip_info("30141"))
 
-print(zipfreeinfo.get_state())
-print(zipfreeinfo.get_county_name())
+
+print(zipfreeinfo.get_zipcode()) # returns string
+print(zipfreeinfo.get_city()) # returns string
+print(zipfreeinfo.get_state()) # returns string
+print(zipfreeinfo.get_state_abbreviation()) # returns string
+print(zipfreeinfo.get_county_fips()) # returns string
+print(zipfreeinfo.get_county_name()) # returns string
+print(zipfreeinfo.get_latitude()) # returns float
+print(zipfreeinfo.get_longitude()) # returns float
